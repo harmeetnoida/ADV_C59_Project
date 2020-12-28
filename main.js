df4887f3b0197931be6d026b305a434212f4b104
harmeetnoida <73604020+harmeetnoida@users.noreply.github.com>
2020-12-28 23:22:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function flipimage() {
+    document.getElementById("img1").src = "https://media.istockphoto.com/photos/education-concept-with-book-in-library-picture-id944631208?k=6&m=944631208&s=612x612&w=0&h=5Hx0ksAT5Majaz40Iov6oLO6GaDn2cxySnNTDTH3Qk8=";
+}
